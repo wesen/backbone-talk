7da32bf4abd2fc81f1ea38407700adeb7efa7625
Manuel Odendahl <wesen@ruinwesen.com>
2011-09-13 17:01:53 +0200

    Add new routes for blog

--- sites/backbone/js/main.js
+++ sites/backbone/js/main.js
@@ -10,7 +10,9 @@ var SINGLE_PAGE_APP = true;
 var PortfolioApp = Backbone.Router.extend({
   routes: {
     "": "index",
+    "index": "index",
     "blog": "blog",
+    "blog/category/:category": "blog",
     "services": "services",
     "about": "about",
     "contact": "contact"
@@ -19,26 +21,28 @@ var PortfolioApp = Backbone.Router.extend({
   initialize: function () {
     tplBaseData.origSite = tplBaseData.site;
     _.bindAll(this);
-    debugLog("main");
+
     App.Collections.testimonials = new Testimonials();
+    App.Collections.blogs = new Blogs();
   },
 
   setView: function (view) {
     if (this.view) {
-      this.view.remove();
+//      this.view.remove();
       this.view.destroy();
       this.view = undefined;
     }
 
     this.view = view;
-    this.view.renderTemplate();
   },
 
   index: function () {
-    this.setView(new App.Views.mainView());
+    this.setView(new App.Views.mainView({el: $("body")}));
   },
 
-  blog: function () {
+  blog: function (category) {
+    this.setView(new App.Views.blogView({el: $("body"),
+                                         category: category}));
   },
 
   services: function () {
@@ -54,5 +58,35 @@ var PortfolioApp = Backbone.Router.extend({
 $(function () {
   var app = new PortfolioApp();
   window.app = app;
-  Backbone.history.start({pushState: true, root: "/backbone/"});
+  Backbone.history.start({root: "/backbone/"});
 });
+
+
+function populateTestimonials() {
+  var t = App.Collections.testimonials.create(
+    {text: "Wicked site",
+     author: "John Henry",
+     city: "Berlin"});
+  t = App.Collections.testimonials.create(
+    {text: "Crazy Stuff",
+     author: "Macademia Wilbert",
+     city: "Cuxhaven"});
+  t = App.Collections.testimonials.create(
+    {text: "I barely managed to keep it together",
+     author: "Quincy Van De Waals",
+     city: "Amsterdam"});
+}
+
+function populateBlogs() {
+  var b = App.Collections.blogs.create(
+    {title: "Today I did something",
+     category: "Hardcore",
+     text: "And it was actually quite fine. No really, you wouldn't believe. It was fun. Barely. But still.",
+     date: new Date()
+    });
+  b = App.Collections.blogs.create(
+    {title: "Yesterday I did something too",
+     category: "Web Design",
+     text: "Add a model (or an array of models) to the collection. Fires an 'add' event, which you can pass {silent: true} to suppress. If a model property is defined, you may also pass raw attributes objects, and have them be vivified as instances of the model. Pass {at: index} to splice the model into the collection at the specified index.",
+     date: new Date()});
+}
